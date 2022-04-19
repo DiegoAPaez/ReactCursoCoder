30eb3d5140e75a20688b1a9aa55dc252eba64fb2
diego.p31 <diego.p31@hotmail.com>
2022-04-19 11:27:16 -0300

    Events

--- src/components/Counter/ItemCount.js
+++ src/components/Counter/ItemCount.js
@@ -1,6 +1,7 @@
 import { useState } from "react";
 
-const ItemCount = ({stock, initial}) => {
+
+const ItemCount = ({onConfirm,stock, initial}) => {
     const [count, setCount] = useState (initial)
     const decrement = () => {
         if (count > initial)
@@ -12,12 +13,13 @@ const ItemCount = ({stock, initial}) => {
         setCount(count + 1)
     }
     return (
-        <div className="flex flex-col items-center w-44 bg-gray-200 my-2 rounded">
-            <div className="bg-white flex flex-row w-1/2 my-2 justify-between items-center rounded">
+        <div className="flex flex-row items-center bg-gray-200 mx-2 rounded">
+            <div className="bg-white flex flex-row w-1/2 m-2 justify-between items-center rounded">
                 <button className="m-1 p-1" onClick={decrement}>-</button>
                 <p className="m-1">{count}</p>
                 <button className="m-1 p-1" onClick={increment}>+</button>
-            </div>  
+            </div>
+            <button onClick={() => onConfirm(count)} className="text-center border-solid text-white bg-green-500 h-12 p-2 my-2 rounded">Añadir al carrito</button>  
         </div>
     )
 }
--- src/components/ItemDetail/ItemDetail.js
+++ src/components/ItemDetail/ItemDetail.js
@@ -1,21 +1,30 @@
+import { useState } from "react"
+import { Link } from "react-router-dom"
 import ItemCount from "../Counter/ItemCount"
 
 const ItemDetail = ({ id, name, image, category, description, price, detail, stock }) => {
+    const [quantity, setQuantity] = useState(0)
+    const handleAdd = (count) => {
+        setQuantity({count})
+        console.log({count})
+    }
+
+
     return (
         <article className="border-solid border-slate-100 bg-gray-200 w-4/5 p-2 mx-2 mt-3 text-center rounded">
             <header className="my-3">
                 <h2 className="font-semibold text-xl">{name}</h2>
             </header>
             <div className="grid justify-items-center content-center">
-                <img className="rounded h-80 row-start-1 row-end-2" src={image} alt={`Imagen: ${name}`}/>
+                <img className="rounded h-80 row-start-1 row-end-3" src={image} alt={`Imagen: ${name}`}/>
                 <section className="row-start-1 row-end-2 mx-3">
                     <p className="my-2 text-justify">Descripción: {description}</p>
                     <p className="my-2 text-sm italic">Categoria: {category}</p>
                     <p className="my-2 text-sm italic">{detail}</p>
                     <p className="my-2 font-semibold">Precio: ${price}</p>
                 </section>           
-                <footer className='col-start-1 col-end-3'>
-                <ItemCount stock={stock} initial={1} className=""/>
+                <footer className='col-start-2 col-end-3'>
+                    {quantity.count > 0 ? <Link to='/cart' className="text-center border-solid text-white bg-green-500 h-12 p-2 my-2 rounded">Ir al carrito</Link> : <ItemCount onConfirm={handleAdd} stock={stock} initial={1} className=""/>}
                 </footer>
             </div> 
         </article>
